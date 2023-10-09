a7145a5e1500910a4f041ebe3b2214d32957da7b
yi.luo <yi.luo@19pay.com>
2023-10-09 14:58:43 +0800

    feat:webpack demo01

--- demo01/webpack.config.js
+++ demo01/webpack.config.js
@@ -1,9 +1,31 @@
+const path = require("path");
 // 引入插件
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+// 清理dist目录插件
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// 复制插件
+const copyPlugin = require("copy-webpack-plugin");
+// 抽离css文件
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// 压缩css文件  开发环境不需要
+const OptimizeCssPlugin = require("optimize-css-assets-webpack-plugin");
+
 const isDev = process.env.NODE_ENV === "development";
 const config = require("./public/config")[isDev ? "dev" : "test"];
 
 module.exports = {
+  // 入口文件 可以是一个字符串  一个数组 一个对象
+  entry: "./src/index.js",
+
+  // 出口配置
+  output: {
+    path: path.resolve(__dirname, "dist"), // 路径
+    // filename: "bundle.js", // 文件名
+    filename: "bundle.[hash:6].js", // 文件名(考虑到缓存问题 加上hash)
+    publicPath: "/",
+  },
+
   // 模式
   mode: isDev ? "development" : "production",
 
@@ -19,6 +41,32 @@ module.exports = {
       //   },
       // hash: true //是否加上hash，默认是 false
     }),
+
+    // 不需要传参数
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ["**/*", "!dll", "!dll/**"], //  不删除dll以及下面的文件
+    }),
+
+    new copyPlugin(
+      [
+        {
+          from: "public/js/*.js",
+          to: path.resolve(__dirname, "dist", "js"),
+          flatten: true, // 此处如果设置为false 会把文件夹路径都拷贝上 顾遥设置为true
+        },
+      ],
+      {
+        ignore: ["other.js"], // 忽略的文件夹数组
+      }
+    ),
+
+    //  抽离css文件
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
+
+    // 压缩css文件
+    new OptimizeCssPlugin(),
   ],
 
   // 模块
@@ -35,17 +83,14 @@ module.exports = {
       {
         test: /\.(le|c)ss$/,
         use: [
-          "style-loader",
+          // "style-loader",
+          MiniCssExtractPlugin.loader,
           "css-loader",
           {
             loader: "postcss-loader",
             options: {
               plugins: function () {
-                return [
-                  require("autoprefixer")({
-                    overrideBrowserslist: [">0.25%", "not dead"],
-                  }),
-                ];
+                return [require("autoprefixer")()];
               },
             },
           },
@@ -63,7 +108,7 @@ module.exports = {
             options: {
               limit: 10240, // 10k
               esModule: false,
-              name:'[name]_[hash:6]_[ext]',
+              name: "[name]_[hash:6].[ext]", // 图片资源名称
               outputPath: "assets", // 本地资源打包文件夹
             },
           },
@@ -84,5 +129,5 @@ module.exports = {
     compress: true, //是否启用 gzip 压缩
   },
 
-  devtool: "cheap-module-eval-source-map", // 开发环境下使用
+  devtool: "source-map", // 开发环境下使用
 };
